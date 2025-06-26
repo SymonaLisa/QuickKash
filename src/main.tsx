94209c3965eff11f379f41c07824679b1721a88e
Virgi Symone Jordaan <virgi.s.jordaan@gmail.com>
2025-06-26 14:00:42 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container missing in index.html');
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
